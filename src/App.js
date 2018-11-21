fa57025638266080212319f879d2e9d02a9c507b
unknown <sebanazarian@gmail.com>
2018-11-21 10:21:11 -0300

    react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import { UserList } from './users';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+
+
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+      {/* <Resource name="users" list={ListGuesser} /> */}
+      <Resource name="users" list={UserList} />
+  </Admin>
+);
+
+export default App;
+
+
+
